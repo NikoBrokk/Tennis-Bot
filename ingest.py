b658da3b28d2179727d4c5fcb2a8a0659a9698f2
NikoBrokk <nikolaibrokhaug@gmail.com>
2025-08-28 14:44:03 +0200

    Create ingest.py

---
+++ ingest.py
@@ -0,0 +1,25 @@
+from PyPDF2 import PdfReader
+
+def load_pdf (path):
+  """Load text per page from a PDF file
+  ARGS: 
+  path (str): Path to PDF file.
+  REturns: 
+  list of str: One string per page of text
+  """
+  reader = PdfReader (path)
+  pages= []
+  for page in reader.pages:
+    text=page.extract_text()
+    if text: #sjekk at det ikke er NONE
+      pages.append(text)
+  return pages
+
+def chunk_text (text, chunk_size=200):
+  "Split text into smaller chunks"
+  return []
+
+if __name__ == "__main__":
+  # TODO: test med en PDF i data/-mappa
+  # print antall sider og antall chunks
+  pass
